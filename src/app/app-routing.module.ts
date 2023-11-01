97b0ebc0537a886d2cc671c9e8fb7e43d1f817ce
johannbitencourt <johann.bitencourt@gmail.com>
2023-11-01 14:54:52 -0300

    feat: first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: async () => (await import('./modules/pokemons/pokemons.module')).PokemonsModule
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
